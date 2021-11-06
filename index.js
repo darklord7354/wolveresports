4a6eeea2fd8e0d0352e853c1532934fd308350ba
darklord7354 <90344737+darklord7354@users.noreply.github.com>
2021-11-06 11:02:47 +0300

    weeeeeeeeeee
    weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ index.js
@@ -0,0 +1,39 @@
+const scrollToTop = document.getElementById("scrollup")
+
+scrollToTop.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
+const scrollToTop1 = document.getElementById("btn2")
+
+scrollToTop1.addEventListener("click", function () {
+    window.scrollTo({
+        top: 325,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
+const scrollToTop2 = document.getElementById("btn1")
+
+scrollToTop2.addEventListener("click", function () {
+    window.scrollTo({
+        top: 1250,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
+const scrollToTop3 = document.getElementById("btn4")
+
+scrollToTop3.addEventListener("click", function () {
+    window.scrollTo({
+        top: 1250,
+        left: 0,
+        behavior: "smooth"
+    })
+})
